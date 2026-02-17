251757510a88e67f89b4d717e7064319339e82c2
Neyo <pperico946@gmail.com>
2026-02-17 17:21:57 +0100

    Reajuste de pagina-11

--- docs/main.js
+++ docs/main.js
@@ -171,7 +171,7 @@ async function loginEmpresa(event) {
     // Guardar token según rol
     if (data.rol === "admin") {
       localStorage.setItem("admin_token", data.token);
-      window.location.href = "/private/admin.html";
+      window.location.href = "https://drepartos.onrender.com/private/admin.html";
     } 
     else if (data.rol === "empleado") {
       localStorage.setItem("empleado_token", data.token);
